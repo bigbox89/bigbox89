0793407b98ef88afa5a84d9dff5a0d958b2c7689
bigbox89@mail.ru <bigbox89@mail.ru>
2022-04-05 16:07:51 +0500

    add war

--- src/main/java/com/github/bigbox89/HelloJenkinsApplication.java
+++ src/main/java/com/github/bigbox89/HelloJenkinsApplication.java
@@ -2,6 +2,7 @@ package com.github.bigbox89;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class HelloJenkinsApplication {
@@ -9,5 +10,10 @@ public class HelloJenkinsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloJenkinsApplication.class, args);
 	}
-
+	// 1. Annotation
+	@RequestMapping("/")
+	// 3. Method that maps to the request route above
+	public String hello() { // 3
+		return "Hello World!";
+	}
 }
